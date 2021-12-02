e6ddfe3700096554856cc0eaf4d7468145d3407a
kai63001 <animehq5@gmail.com>
2021-12-02 14:58:56 +0700

    fix addChile invalid 😂

--- js/darkmode.content.js
+++ js/darkmode.content.js
@@ -1,18 +1,34 @@
+const head =
+  document.documentElement || document.head || document.querySelector("head");
+
 const customSite = (site) => {
   const siteList = {
     "wikipedia.org": "wikipedia",
   };
   for (const [key, value] of Object.entries(siteList)) {
-    if(site.indexOf(key) >= 0){
-        return value;
+    if (site.indexOf(key) >= 0) {
+      return value;
     }
   }
-  return '';
+  return "";
+};
+
+const linkCreate = (path) => {
+  var link = document.createElement("link"),
+    href = chrome.runtime.getURL("css/" + path + ".css");
+  link.setAttribute("type", "text/css");
+  link.setAttribute("id", path + "-custom-css");
+  link.setAttribute("class", "dmn-custom-append-data");
+  link.setAttribute("rel", "stylesheet");
+  link.setAttribute("href", href);
+  if (head) {
+    head.appendChild(link);
+  }
 };
 
 const main = () => {
   const nowUrl = window.location.hostname;
-  console.log(customSite(nowUrl));
+  linkCreate(customSite(nowUrl));
 };
 
 main();
