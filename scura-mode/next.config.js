a23f91afe6ad8ce00144010ffc4d1cd91a710b71
kai63001 <animehq5@gmail.com>
2021-12-02 17:21:09 +0700

    update

--- scura-mode/next.config.js
+++ scura-mode/next.config.js
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   target: 'serverless',
   exportPathMap: () => ({
-    "/": {
-      page: "/web",
+    "/scura-mode": {
+      page: "/scura-mode",
     },
   }),
   images: {
