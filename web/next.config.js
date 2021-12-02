d0e91007e5473b65630ca36736402d7c31315261
kai63001 <animehq5@gmail.com>
2021-12-02 17:38:19 +0700

    update

--- web/next.config.js
+++ web/next.config.js
@@ -2,11 +2,6 @@
 module.exports = {
   assetPrefix: "/scura-mode",
   target: 'serverless',
-  exportPathMap: () => ({
-    "/index": {
-      page: "/",
-    },
-  }),
   images: {
     loader: 'imgix',
     path: '/web',
