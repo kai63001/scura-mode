992ef5ca145c9e0b62a08975ffdd0c619dd68c30
kai63001 <animehq5@gmail.com>
2021-12-02 17:02:29 +0700

    fix ci

--- web/next.config.js
+++ web/next.config.js
@@ -3,12 +3,12 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   target: 'serverless',
   exportPathMap: () => ({
-    "/web": {
-      page: "/web",
+    "/": {
+      page: "/",
     },
   }),
   images: {
     loader: 'imgix',
-    path: ""
+    path: "/web"
   },
 }
