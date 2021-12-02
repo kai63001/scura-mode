42620e84e906ad5f74fe5299e05813e66ba27d6b
kai63001 <animehq5@gmail.com>
2021-12-02 13:52:55 +0700

    add custom site 🚀

--- js/darkmode.content.js
+++ js/darkmode.content.js
@@ -1,2 +1,18 @@
-const nowUrl = window.location.hostname;
-console.log(nowUrl);
+const customSite = (site) => {
+  const siteList = {
+    "wikipedia.org": "wikipedia",
+  };
+  for (const [key, value] of Object.entries(siteList)) {
+    if(site.indexOf(key) >= 0){
+        return value;
+    }
+  }
+  return '';
+};
+
+const main = () => {
+  const nowUrl = window.location.hostname;
+  console.log(customSite(nowUrl));
+};
+
+main();
