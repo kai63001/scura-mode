ac71738d4a7594c7a84ae06c5e869e5c7102afb9
kai63001 <animehq5@gmail.com>
2021-12-02 17:17:02 +0700

    fix it

--- web/next.config.js
+++ web/next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   target: 'serverless',
   exportPathMap: () => ({
-    "/web": {
+    "/": {
       page: "/scura-mode",
     },
   }),
