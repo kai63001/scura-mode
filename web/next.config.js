38d23317de8f60df9dfb2944cd20f86dc63b3c92
kai63001 <animehq5@gmail.com>
2021-12-03 21:21:46 +0700

    basePath update

--- web/next.config.js
+++ web/next.config.js
@@ -2,6 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   assetPrefix: isProd ? "/scura-mode" : "",
+  basePath: isProd ? "/scura-mode" : "",
   target: "serverless",
   images: {
     loader: "imgix",
