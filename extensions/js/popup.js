7162c07769e7b4f76aea0b898bb6be687a183629
kai63001 <animehq5@gmail.com>
2021-12-05 19:40:50 +0700

    update popup checkbox onloaded

--- extensions/js/popup.js
+++ extensions/js/popup.js
@@ -1,6 +1,17 @@
 let onOrOff = true;
 
 window.onload = function () {
+  chrome.storage.sync.get(["onOrOff"], function (result) {
+    if (result.onOrOff == undefined) {
+      onOrOff = true;
+    } else {
+      onOrOff = result.onOrOff;
+    }
+    if (!onOrOff) {
+      document.getElementsByTagName("body")[0].classList.add("white");
+      document.getElementById("checkbox").checked = false;
+    }
+  });
   document.getElementById("onoff").onclick = function () {
     if (onOrOff) {
       document.getElementsByTagName("body")[0].classList.add("white");
